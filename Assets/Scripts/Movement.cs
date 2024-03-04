97577c2355d1579cf65464115508114cd4a0a21b
Sami <sami-la98@hotmail.fr>
2024-03-04 16:46:12 +0100

    creating platform tilt

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,22 +1,65 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
     [SerializeField]
-   private float speed = 0;
+    public float speed = 30f;
 
-    // Update is called once per frame
     void Update()
     {
-        float moveX = Input.GetAxis("Horizontal");  
-        float moveY = Input.GetAxis("Vertical"); 
+        float moveX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float moveY = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        
+        transform.Rotate(-moveY, 0f, moveX, Space.World);
+    }
 
-        Vector3 movement = transform.right * moveX + transform.left * moveY;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player")) 
+        {
+            GameManager.Instance.SetLastPosition(transform.position);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collision avec " + collision.gameObject.name);
+    }
+}
 
 
-        transform.Translate(movement * Time.deltaTime * speed);
-        //transform.Rotate();
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    public GameObject ballPrefab;
+    private Vector3 lastCheckpointPosition;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void SetLastPosition(Vector3 position)
+    {
+        lastCheckpointPosition = position;
+    }
+
+    public void Respawn()
+    {
+        if (ballPrefab != null)
+        {
+            Instantiate(ballPrefab, lastCheckpointPosition, Quaternion.identity);
+        }
     }
 }
